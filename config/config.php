1da23fb80152f1eed9bf0e1f9075a62c967a298b
danny <65585589+ziac007@users.noreply.github.com>
2024-01-17 07:20:48 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,17 @@
 <?php
-$connection = new PDO("mysql:host=localhost;dbname=book_mytrip", "root", "");
+$host = "tripwebapp-server.postgres.database.azure.com"; // Replace with your Azure database server name
+$dbname = "tripwebapp-database"; // Replace with your database name
+$username = "slnundsogu@tripwebapp-server"; // Replace with your Azure database username
+$password = "#passwordis01"; // Replace with your Azure database password
 
-if (!$connection) {
-  echo " unable connect to data base";
+try {
+    $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    // Set PDO to throw exceptions on error
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected to the database successfully!";
+} catch (PDOException $e) {
+    echo "Unable to connect to the database: " . $e->getMessage();
 }
-  
 ?>
